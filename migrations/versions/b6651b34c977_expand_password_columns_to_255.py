ffe584c0598cecbe0f5dcb716353d2709deafcee
Ceryn Griff <a1ceryn@aol.com>
2025-08-29 10:11:59 +0100

    Expand password columns to 255

---
+++ migrations/versions/b6651b34c977_expand_password_columns_to_255.py
@@ -0,0 +1,24 @@
+"""Expand password columns to 255
+
+Revision ID: b6651b34c977
+Revises: fc04238e494b
+Create Date: 2025-08-29 10:11:34.705987
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b6651b34c977'
+down_revision = 'fc04238e494b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
